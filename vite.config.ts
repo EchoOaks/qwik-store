ef446a3f17160985f4660462c00edd20df5f62f6
EchoOaks <theyappstore@gmail.com>
2024-09-03 14:19:54 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,14 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig(() => {
   return {
     plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    ssr: {
+      noExternal: ['@builder.io/qwik-city'], // Exclude this package from being externalized during SSR build
+    },
+    build: {
+      rollupOptions: {
+        external: ['node:async_hooks'], // Treat this Node.js built-in module as external
+      },
+    },
     preview: {
       headers: {
         "Cache-Control": "public, max-age=600",
